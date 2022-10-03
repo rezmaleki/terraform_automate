d7d6a29ec8260d1e8cbcb0d4695dbc699b8aa636
rezmaleki <32066178+rezmaleki@users.noreply.github.com>
2022-10-03 15:12:56 +0200

    Delete subnet.tf

--- subnet.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_subnet" "core-subnet-private-1" {
-    vpc_id = "${aws_vpc.core-vpc.id}"
-    cidr_block = "${var.private_subnet_1_cidr}"
-    availability_zone = "us-west-2a"
-    tags = {
-        Name = "Harmonelo-core-subnet-private-1"
-    }
-}
-
