924d2f324f9db9a9ff820750679d40aad1965732
rezmaleki <32066178+rezmaleki@users.noreply.github.com>
2022-10-03 15:19:23 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,20 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
+provider "aws" {
+  region = terraform.workspace == "default" ? "us-east-1" : "us-west-2"
 }
 
-#provider "aws" {
-#  region  = "us-west-2"
-#}
+#Get Linux AMI ID using SSM Parameter endpoint in us-east-1
+data "aws_ssm_parameter" "linuxAmi" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
 
-resource "aws_instance" "app_server" {
-
-  ami                         = "ami-017fecd1353bcc96e"
-  instance_type               = "t2.micro"
+#Create and bootstrap EC2 in us-east-1
+resource "aws_instance" "ec2-vm" {
+  ami                         = data.aws_ssm_parameter.linuxAmi.value
+  instance_type               = "t3.micro"
   associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.sg.id]
+  subnet_id                   = aws_subnet.subnet.id
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "${terraform.workspace}-ec2"
   }
-
 }
