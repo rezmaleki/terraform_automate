65d24dc3bfb5e2f8961625b4603d711f86aaaa3f
rezmaleki <32066178+rezmaleki@users.noreply.github.com>
2022-10-03 14:48:50 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,28 @@
-resource "null_resource" "example" {
-       triggers = {
-         value = "A example resource that does nothing!"
-       }
-     }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+#provider "aws" {
+#  region  = "us-west-2"
+#}
+
+resource "aws_instance" "app_server" {
+
+  ami                         = "ami-017fecd1353bcc96e"
+  vpc_security_group_ids      = [aws_security_group.ec2-sg.id]
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.my_instance_key_pair.key_name
+  subnet_id                   = "${aws_subnet.core-subnet-public-1.id}"
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+
+}
